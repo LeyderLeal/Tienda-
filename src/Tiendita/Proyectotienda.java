f988e31e1d845656bbd51903baea822a0ad1030b
LeyderLeal <leyder.leal2005@gmail.com>
2023-10-24 15:13:08 -0500

    firts commit

---
+++ src/Tiendita/Proyectotienda.java
@@ -0,0 +1,19 @@
+
+package Tiendita;
+
+/**
+ *
+ * @author SENA
+ */
+public class Proyectotienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        tienda frm = new tienda();
+        frm.setVisible(true);
+    }
+    
+}
